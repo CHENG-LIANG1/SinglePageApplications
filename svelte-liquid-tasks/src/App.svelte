7a7f32cc284d9850d1576a04ceed36f40670d57a
Cheng <madfoxfoxy@gmail.com>
2026-01-23 00:51:47 +0800

    Add Svelte project for liquid tasks

---
+++ svelte-liquid-tasks/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from 'svelte'
+  import legacyTemplate from './lib/legacy-template.html?raw'
+  import { mountLegacyApp } from './lib/legacy'
+
+  onMount(() => {
+    mountLegacyApp()
+  })
+</script>
+
+<div>{@html legacyTemplate}</div>
